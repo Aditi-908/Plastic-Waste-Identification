1a550839c87aa586462688bff1e9ed42f8d7f254
Aditi-908 <81457580+Aditi-908@users.noreply.github.com>
2024-01-08 15:16:09 +0530

    Update cam.py

--- major_project/cam.py
+++ major_project/cam.py
@@ -1,6 +1,6 @@
 import cv2
 import os
-import getpass
+
 
 def save_frame_camera_key(device_num, dir_path, basename, ext='jpg', delay=1, window_name='frame'):
     cap = cv2.VideoCapture(device_num)
@@ -16,18 +16,15 @@ def save_frame_camera_key(device_num, dir_path, basename, ext='jpg', delay=1, wi
         ret, frame = cap.read()
         cv2.imshow(window_name, frame)
         key = cv2.waitKey(delay) & 0xFF
-        
         if key == ord('c'):
             print("Image captured")
             print(base_path)
-            cv2.imwrite(f'{base_path}_{n}.{ext}', frame)
+            cv2.imwrite('{}_{}.{}'.format(base_path, n, ext), frame)
             n += 1
         elif key == ord('q'):
             break
 
     cv2.destroyWindow(window_name)
 
-if __name__ == "__main__":
-    username = getpass.getuser()
-    test_dir = f'/Users/{username}/Downloads/major_project/test'  # Adjust the path as needed
-    save_frame_camera_key(0, test_dir, 'camera_capture')
+
+save_frame_camera_key(0, f'C:\\Aditi\\major_project\\test', 'camera_capture')
